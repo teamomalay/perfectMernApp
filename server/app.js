0d0ee949b98e7fb296ab504c00bdc1aca7bf264d
Malay <malaytailor5@gmail.com>
2021-09-01 00:34:54 +0530

    first commit

--- server/app.js
+++ server/app.js
@@ -1,50 +1,26 @@
 const dotenv = require("dotenv");
-const mongoose = require('mongoose');
-const express = require('express');
+dotenv.config({ path: "./config.env" });
+
+const mongoose = require("mongoose");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+
 const app = express();
+app.use(cookieParser());
 
-dotenv.config({ path: './config.env' });
-
-require('./db/conn');
-// const User = require('./model/userSchema');
+require("./db/conn");
 
 app.use(express.json());
 
-// we link the router files to make our route easy 
-app.use(require('./router/auth'));
+// we link the router files to make our route easy
+app.use(require("./router/auth"));
 
 const PORT = process.env.PORT;
 
-
-// Middelware 
-const middleware = (req,res, next) => {
-    console.log(`Hello my Middleware`);
-    next();
-}
-
-// app.get('/', (req, res) => {
-//     res.send(`Hello world from the server app.js`);
-// });
-
-app.get('/about', middleware, (req, res) => {
-    console.log(`Hello my About`);
-    res.send(`Hello About world from the server`);
-});
-
-app.get('/contact', (req, res) => {
-    res.send(`Hello Contact world from the server`);
-});
-
-app.get('/signin', (req, res) => {
-    res.send(`Hello Login world from the server`);
-});
-
-app.get('/signup', (req, res) => {
-    res.send(`Hello Registration world from the server`);
+app.get("/signup", (req, res) => {
+  res.send(`Hello Registration world from the server`);
 });
 
 app.listen(PORT, () => {
-    console.log(`server is runnig at port no ${PORT}`);
-})
-
-
+  console.log(`server is running at port no ${PORT}`);
+});
--- server/router/auth.js
+++ server/router/auth.js
@@ -1,14 +1,153 @@
-const express = require('express');
+const jwt = require("jsonwebtoken");
+const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const authenticate = require("../middleware/authenticate");
 
-router.get('/', (req, res) => {
-    res.send(`Hello world from the server rotuer js`);
+require("../db/conn");
+const User = require("../model/userSchema");
+
+router.get("/", (req, res) => {
+  res.send(`Hello world from the server router js`);
 });
 
-router.post('/register', (req, res) => {
-    console.log(req.body);
-    res.json({ message: req.body });
-    // res.send("mera register page");
+// using promises
+
+// router.post('/register', (req, res) => {
+
+//     const { name, email, phone, work, password, cpassword} = req.body;
+
+//     if (!name || !email || !phone || !work || !password || !cpassword) {
+//         return res.status(422).json({ error: "Plz filled the field properly" });
+//     }
+
+//     User.findOne({ email: email })
+//         .then((userExist) => {
+//             if (userExist) {
+//                 return res.status(422).json({ error: "Email already Exist" });
+//             }
+
+//             const user = new User({ name, email, phone, work, password, cpassword });
+
+//             user.save().then(() => {
+//                 res.status(201).json({ message: "user registered successfuly" });
+//             }).catch((err) => res.status(500).json({ error: "Failed to registered" }));
+
+//         }).catch(err => { console.log(err); });
+
+// });
+
+// Async-Await
+
+router.post("/register", async (req, res) => {
+  const { name, email, phone, work, password, cpassword } = req.body;
+
+  if (!name || !email || !phone || !work || !password || !cpassword) {
+    return res.status(422).json({ error: "Plz fill the fields properly" });
+  }
+
+  try {
+    const userExist = await User.findOne({ email: email });
+
+    if (userExist) {
+      return res.status(422).json({ error: "Email already Exist" });
+    } else if (password !== cpassword) {
+      return res.status(422).json({ error: "password are not matching" });
+    } else {
+      const user = new User({ name, email, phone, work, password, cpassword });
+      // yeha pe
+      await user.save();
+      res.status(201).json({ message: "user registered successfuly" });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// login route
+
+router.post("/signin", async (req, res) => {
+  try {
+    let token;
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ error: "Plz Fill the data" });
+    }
+
+    const userLogin = await User.findOne({ email: email });
+
+    if (userLogin) {
+      const isMatch = await bcrypt.compare(password, userLogin.password);
+
+      if (!isMatch) {
+        res.status(400).json({ error: "Invalid Credientials " });
+      } else {
+        // need to genereate the token and stored cookie after the password match
+
+        token = await userLogin.generateAuthToken();
+        console.log(token);
+
+        res.cookie("jwtoken", token, {
+          expires: new Date(Date.now() + 25892000000),
+          httpOnly: true,
+        });
+
+        res.json({ message: "user Signin Successfully" });
+      }
+    } else {
+      res.status(400).json({ error: "Invalid Credientials " });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// about us page
+
+router.get("/about", authenticate, (req, res) => {
+  res.send(req.rootUser);
+});
+
+// get user data for contact us and home page
+router.get("/getdata", authenticate, (req, res) => {
+  res.send(req.rootUser);
+});
+
+// contact us page
+
+router.post("/contact", authenticate, async (req, res) => {
+  try {
+    const { name, email, phone, message } = req.body;
+
+    if (!name || !email || !phone || !message) {
+      console.log("error in contact form");
+      return res.json({ error: "plzz filled the contact form " });
+    }
+
+    const userContact = await User.findOne({ _id: req.userID });
+
+    if (userContact) {
+      const userMessage = await userContact.addMessage(
+        name,
+        email,
+        phone,
+        message
+      );
+
+      await userContact.save();
+
+      res.status(201).json({ message: "user Contact successfully" });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// Logout  ka page
+router.get("/logout", (req, res) => {
+  res.clearCookie("jwtoken", { path: "/" });
+  res.status(200).send("User logout");
 });
 
 module.exports = router;
